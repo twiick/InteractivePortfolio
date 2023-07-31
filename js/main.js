980407fb2d8b9a55bab9b99d235315e0d335ad02
twiick <twiick.com@gmail.com>
2023-07-30 19:50:42 -0500

    Locked screen, Login Screen, Languages context menu and Websites context menu has been finished

--- js/main.js
+++ js/main.js
@@ -1,14 +1,83 @@
 const lockedScreen=document.querySelector("#locked-screen");
 const backgroundsPath="../assets/images/pictures/";
 const background1="background_1.jpg";
+const background2="background_2.jpg";
 const lockedScreenBlur=document.querySelector("#locked-screen-blur");
+const loginContainer=document.querySelector("#login-container");
 const loginInput=document.querySelector("#login-input");
 const loginInputIcon=document.querySelector("#login-input-icon");
+const screenFloatMenu=document.querySelector("#screen-float-menu");
+const languageIcon=document.querySelector("#language-icon");
+const languagesContextMenu=document.querySelector("#languages-context-menu");
+const currentLanguageName=document.querySelector("#current-language-name");
+const currentLanguageRegion=document.querySelector("#current-language-region");
+const websitesIcon=document.querySelector("#website-icon");
+const websitesContextMenu=document.querySelector("#websites-context-menu");
+const desktop=document.querySelector("#desktop");
+
+desktop.style.backgroundImage=`url(${backgroundsPath}${background2})`
+
+languageIcon.addEventListener("click",()=>{
+    languagesContextMenu.classList.toggle("disable");
+    if(websitesContextMenu.classList.contains("disable")==false){
+        websitesContextMenu.classList.toggle("disable");
+        websitesContextMenu.classList.toggle("invisible");
+    }
+    languagesContextMenu.classList.remove("disable");
+    setTimeout(() => {
+        languagesContextMenu.classList.remove("invisible");
+    }, 1);
+})
+
+websitesIcon.addEventListener("click",()=>{
+    websitesContextMenu.classList.toggle("disable");
+    if(languagesContextMenu.classList.contains("disable")==false){
+        languagesContextMenu.classList.toggle("disable");
+        languagesContextMenu.classList.toggle("invisible");
+    }
+    websitesContextMenu.classList.remove("disable");
+    setTimeout(() => {
+        websitesContextMenu.classList.remove("invisible");
+    }, 1);
+})
 
 lockedScreen.style.backgroundImage=`url(${backgroundsPath}/${background1})`;
 lockedScreen.addEventListener("click",()=>{
-    lockedScreenBlur.classList.add("blur")
+    lockedScreenBlur.classList.add("blur");
+    loginContainer.classList.remove("disable");
+    setTimeout(() => {
+        loginContainer.classList.remove("invisible");
+    }, 1);
+    if(languagesContextMenu.classList.contains("disable")==false){
+        languagesContextMenu.classList.toggle("invisible");
+        setTimeout(() => {
+            languagesContextMenu.classList.toggle("disable");
+        }, 500);
+    }
+    if(websitesContextMenu.classList.contains("disable")==false){
+        websitesContextMenu.classList.toggle("invisible");
+        setTimeout(() => {
+            websitesContextMenu.classList.toggle("disable");
+        }, 500);
+    }
+    screenFloatMenu.classList.remove("disable")
+    setTimeout(() => {
+        screenFloatMenu.classList.remove("invisible")
+    }, 1);
 })
+
+loginInput.addEventListener('keydown', (event)=>{
+    if (event.key === 'Enter') {
+        lockedScreen.classList.add("invisible");
+        loginContainer.classList.add("invisible");
+        screenFloatMenu.classList.add("invisible");
+        setTimeout(() => {
+            lockedScreen.classList.add("disable");
+            loginContainer.classList.add("disable");
+            screenFloatMenu.classList.add("disable");
+        }, 500);
+    }
+  });
 
 loginInputIcon.addEventListener("click",()=>{
     loginInputIcon.classList.toggle("fa-eye");
@@ -38,7 +107,6 @@ langPt.addEventListener("click",()=>{changeLanguage(langPt,"pt");});
 // Create a function to change language
 // Receive as Parameter (Object)selected lang button and (String)lang abbreviation
 function changeLanguage(selectedlangButton,newLanguage){
-
     // Create an if to validate if the selected lang button has the "active" class
     if(selectedlangButton.classList.contains("active")==false){
 
@@ -56,23 +124,23 @@ function changeLanguage(selectedlangButton,newLanguage){
         // Create a switch conditional to compare the (String)lang abbreviation, and return a function to set the new language
         switch (newLanguage) {
             case "en":
-                changeContentLanguage("Languages","English (United States)","Spanish (Colombia)","Italian (Italia)","Portugues (Brasil)","Enter your password");
+                changeContentLanguage("Languages","English (United States)","Spanish (Colombia)","Italian (Italia)","Portugues (Brasil)","Enter your password","ENG","USA");
                 break;
             case "es":
-                changeContentLanguage("Idiomas","Inglés (Estados Unidos)","Español (Colombia)","Italiano (Italia)","Portugués (Brasil)", "Ingresa tu contraseña");
+                changeContentLanguage("Idiomas","Inglés (Estados Unidos)","Español (Colombia)","Italiano (Italia)","Portugués (Brasil)", "Ingresa tu contraseña","ESP","COL");
                 break;
             case "it":
-                changeContentLanguage("Lingue","Inglese (Stati Uniti)","Spagnolo (Colombia)","Italiano (Italia)","Portoghese (Brasile)","Inserisci la tua password");
+                changeContentLanguage("Lingue","Inglese (Stati Uniti)","Spagnolo (Colombia)","Italiano (Italia)","Portoghese (Brasile)","Inserisci la tua password","ITA","ITA");
                 break;
             case "pt":
-                changeContentLanguage("Línguas","Inglês (Estados Unidos)","Espanhol (Colômbia)","Italiano (Itália)","Portugues (Brasil)","Coloque sua senha");
+                changeContentLanguage("Línguas","Inglês (Estados Unidos)","Espanhol (Colômbia)","Italiano (Itália)","Portugues (Brasil)","Coloque sua senha","POR","BRA");
                 break;
         }
     }
 }
 
 // Create a function to find the elements who change the language, and set the new language, using innerText method
-function changeContentLanguage(langTitle,lanInfoEng,lanInfoSpa,lanInfoIta,lanInfoPor,loginInputPlaceholder){
+function changeContentLanguage(langTitle,lanInfoEng,lanInfoSpa,lanInfoIta,lanInfoPor,loginInputPlaceholder,langAbb,regionAbb){
     const languagesTitle=document.querySelector(".languages-title");
     const langInfoEnglish=document.querySelector(".lang-info-english");
     const langInfoSpanish=document.querySelector(".lang-info-spanish");
@@ -84,4 +152,6 @@ function changeContentLanguage(langTitle,lanInfoEng,lanInfoSpa,lanInfoIta,lanInf
     langInfoItalian.innerText=lanInfoIta;
     langInfoPortugues.innerText=lanInfoPor;
     loginInput.placeholder=loginInputPlaceholder;
+    currentLanguageName.innerText=langAbb;
+    currentLanguageRegion.innerText=regionAbb;
 }
